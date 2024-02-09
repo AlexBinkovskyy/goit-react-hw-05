4d4eee3832956b931a8e5be947f80eb5ae721f2c
AlexBinkovskyy <alex_bin@ukr.net>
2024-02-09 22:59:54 +0200

    start

--- src/components/LoadMoreBtn/LoadMoreBtn.jsx
+++
@@ -1,5 +0,0 @@
-import style from './LoadMoreBtn.module.css'
-
-export const LoadMoreBtn = ({onClick}) => {
-    return <button type="submit" onClick={onClick} className={style.button}>Load more images</button>
-};
