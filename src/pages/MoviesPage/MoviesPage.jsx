29ba8ca0b11c038cc002ffb2384e213500f48a6e
AlexBinkovskyy <alex_bin@ukr.net>
2024-02-11 17:43:20 +0200

    css

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,13 +1,16 @@
 import { useId } from 'react';
+import css from './MoviesPage.module.css';
 
 export const MoviesPage = () => {
   const inputId = useId();
 
   return (
-    <div>
-      <label htmlFor={inputId}>
-        <input type="text" id={inputId} />
+    <div className={css.wrapper}>
+      <label htmlFor={inputId} className={css.label}>
+        Search by keyword
       </label>
+      <input type="text" id={inputId} className={css.input}/>
+
       <button type="submit">Search</button>
     </div>
   );
